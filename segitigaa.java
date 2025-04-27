6f6b0eebde2afa611a2914e2d60f3c58ee2da0aa
SalwaKhairunnisa <149360503+salwa1k2@users.noreply.github.com>
2025-04-27 23:18:06 +0700

    segitigaa.java

--- segitigaa.java
+++ segitigaa.java
@@ -13,42 +13,30 @@ public class segitigaa extends ObjekGeometris {
     private double sisi2 = 1.0;
     private double sisi3 = 1.0;
 
-    // Konstruktor tanpa argumen
     public segitigaa() {
     }
-
-    // Konstruktor dengan tiga sisi
     public segitigaa(double sisi1, double sisi2, double sisi3) {
         this.sisi1 = sisi1;
         this.sisi2 = sisi2;
         this.sisi3 = sisi3;
     }
-
-    // Getter
     public double getSisi1() {
         return sisi1;
     }
-
     public double getSisi2() {
         return sisi2;
     }
-
     public double getSisi3() {
         return sisi3;
     }
-
-    // Menghitung luas segitiga
     public double getArea() {
         double s = getPerimeter() / 2;
         return Math.sqrt(s * (s - sisi1) * (s - sisi2) * (s - sisi3));
     }
-
-    // Menghitung keliling segitiga
     public double getPerimeter() {
         return sisi1 + sisi2 + sisi3;
     }
-
-    // Override metode toString
+    
     @Override
     public String toString() {
         return "Segitiga: sisi1 = " + sisi1 + " sisi2 = " + sisi2 + " sisi3 = " + sisi3;
